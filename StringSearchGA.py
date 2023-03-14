4269ec1f9ce86a263542cece4aabfdce5cb1854a
Racemuis <chiara.thoeni@ru.nl>
2023-03-14 17:50:28 +0100

    add exercise 4.4.5

--- StringSearchGA.py
+++ StringSearchGA.py
@@ -2,17 +2,90 @@ from typing import Union, Tuple
 import string
 
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 def main():
     # set hyper parameters
-    target_string = np.array(list("target string"))
+    target_string = np.array(list("long target string"))
     K = 2
-    N = 1000
+    N = 200
     p_crossover = 1
-    mutation_rate = 0.01
+    mutation_rate = 0.1
     alphabet = np.array(list(string.ascii_lowercase) + [" "])
+    n_runs = 20
+    matplotlib.rcParams.update({'font.size': 20})
 
+    # run the algorithm: exercise 4.1 - 4.3
+    #exercise_4_1__4_3(K, N, alphabet, mutation_rate, n_runs, target_string)
+
+    # run the algorithm: exercise 4.4 - 4.5
+    exercise_4_4__4_5(K, N, alphabet, n_runs, target_string)
+
+
+def exercise_4_4__4_5(K, N, alphabet, n_runs, target_string):
+    mutation_rates = np.arange(start=0, stop=0.2, step=0.02)
+    results = np.empty((mutation_rates.shape[0], n_runs))
+    for i, mu in enumerate(tqdm(mutation_rates, desc="Running string search GA")):
+        for j in range(n_runs):
+            results[i, j] = string_search_GA(
+                target_string=target_string, K=K, N=N, mutation_rate=mu, alphabet=alphabet, verbose=False
+            )
+    mean = np.mean(results, axis=1)
+    std = np.std(results, axis=1)
+    plt.plot(mutation_rates, mean, label="mean")
+    plt.fill_between(
+        x=mutation_rates,
+        y1=mean - std,
+        y2=mean + std,
+        alpha=0.2,
+        label="standard deviation (1σ)",
+    )
+    plt.legend()
+    plt.xlabel("Mutation rate")
+    plt.ylabel("Finish time (number of generations)")
+    plt.title(f"GA performance using different values of mu\n"
+              f"with N={N}, K={K}, and n_runs={n_runs}\n"
+              f"length of target string = {target_string.shape[0]} characters")
+    plt.show()
+
+
+def exercise_4_1__4_3(K, N, alphabet, mutation_rate, n_runs, target_string):
+    runs = [
+        string_search_GA(
+            target_string=target_string, K=K, N=N, mutation_rate=mutation_rate, alphabet=alphabet, verbose=False
+        )
+        for i in tqdm(range(n_runs), desc="Running string search GA")
+    ]
+    # plot the resulting distribution
+    plt.boxplot(runs)
+    plt.title(f"Boxplot of the distribution of the finish times over {n_runs} runs\n"
+              f"with N={N}, K={K}, and mu={mutation_rate}\n"
+              f"length of target string = {target_string.shape[0]} characters")
+    plt.ylabel("Finish time (number of generations)")
+    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+    plt.show()
+
+
+def string_search_GA(
+    target_string: np.ndarray, K: int, N: int, mutation_rate: float, alphabet: np.ndarray, verbose: bool = True
+) -> int:
+    """
+    The string search GA as described in the fourth Natural Computing lecture.
+
+    Args:
+        target_string (np.ndarray): The target string represented in an array.
+        K (int): The number of individuals to select for the parental tournament selection.
+        N (int): The population size.
+        mutation_rate (float): The pointwise mutation rate of the individuals.
+        alphabet (np.ndarray): All possible characters that can occur in the target string.
+        verbose (bool): If True, prints the best fittest individual per generation, default = False.
+
+    Returns:
+        int: The number of generations the GA needed to find the target string.
+    """
     # initialize population
     idxs = np.random.randint(low=0, high=alphabet.shape[0], size=(N, target_string.shape[0]))
     population = alphabet[idxs]
@@ -24,8 +97,9 @@ def main():
     # create a list for fast indexing
     idx_grid = np.linspace(start=0, stop=N, num=N, endpoint=False)
 
-    print("Fittest individual within the population:")
-    while not target_found:
+    if verbose:
+        print("Fittest individual within the population:")
+    while not target_found and n_generations < 100:
         # determine the fitness of every solution in the population
         fitness = calculate_fitness(population, target_string)
 
@@ -38,7 +112,7 @@ def main():
             # generate the children
             c_one, c_two = crossover(one=p_one, two=p_two)
             children[i, :] = mutate(c_one, rate=mutation_rate, alphabet=alphabet)
-            children[i+1, :] = mutate(c_two, rate=mutation_rate, alphabet=alphabet)
+            children[i + 1, :] = mutate(c_two, rate=mutation_rate, alphabet=alphabet)
 
         # update the generation number
         n_generations += 1
@@ -47,12 +121,15 @@ def main():
         target_found = 1 in fitness
 
         # paste fittest target on stdout
-        print(f"\t{population[np.argmax(fitness)]}, fitness: {np.max(fitness):.2f}")
+        if verbose:
+            print(f"\t{population[np.argmax(fitness)]}, fitness: {np.max(fitness):.2f}")
 
         # update the population
         population = children
 
-    print(f"Target found in {n_generations} generations.")
+    if verbose:
+        print(f"Target found in {n_generations} generations.")
+    return n_generations
 
 
 def mutate(one: np.ndarray, rate: float, alphabet: np.ndarray) -> np.ndarray:
